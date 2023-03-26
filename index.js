691120005150c58dc3a4d0a02b810eccf7bd9bb6
kaleab tadewos ero <tadewoskaleab@gmail.com>
2023-03-26 18:02:26 -0500

    update port

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express(); 
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
     res.send("Hello World");
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server is Running !!!")
 });
